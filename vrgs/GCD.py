bc2bb6d2a34f68dafacffdee92cb88782ce41772
Satyaki Sikdar <ssikdar@nd.edu>
2018-09-09 23:36:37 -0400

    added ORCA executable and modified GCD to use ORCA instead of RAGE

--- vrgs/GCD.py
+++ vrgs/GCD.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import os
 import platform
 import subprocess
 import pandas as pd
@@ -6,13 +7,36 @@ import scipy.stats
 import math
 import numpy as np
 
-def GCD(h1, h2):
-    df_g = external_rage(h1, 'Orig')
-    df_h = external_rage(h2, 'Test')
+def GCD(h1, h2, mode='rage'):
+    if mode == 'rage':
+        df_g = external_rage(h1, 'Orig')
+        df_h = external_rage(h2, 'Test')
+    else:
+        df_g = external_orca(h1, 'orig')
+        df_h = external_orca(h2, 'test')
     gcm_g = tijana_eval_compute_gcm(df_g)
     gcm_h = tijana_eval_compute_gcm(df_h)
     gcd = tijana_eval_compute_gcd(gcm_g, gcm_h)
     return round(gcd, 3)
+
+
+def external_orca(g, gname):
+    g = max(nx.connected_component_subgraphs(g), key=len)
+    g = nx.convert_node_labels_to_integers(g, first_label=0)
+
+    file_dir = './tmp'
+    with open('{}/{}.in'.format(file_dir, gname), 'w') as f:
+        f.write('{} {}\n'.format(g.order(), g.size()))
+        for u, v in g.edges_iter():
+            f.write('{} {}\n'.format(u, v))
+
+    args = './orca', '4', '{}/{}.in'.format(file_dir, gname), '{}/{}.out'.format(file_dir, gname)
+
+    popen = subprocess.Popen(args, stdout=subprocess.DEVNULL)
+    popen.wait()
+
+    df = pd.read_csv('{}/{}.out'.format(file_dir, gname), sep=' ', header=None)
+    return df
 
 
 def external_rage(G, netname):
@@ -31,10 +55,9 @@ def external_rage(G, netname):
     else:
         args = ("./RAGE.dms", tmp_file)
 
-    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     popen.wait()
-    output = popen.stdout.read()
 
     # Results are hardcoded in the exe
     df = pd.read_csv('./Results/UNDIR_RESULTS_tmp_{}.csv'.format(netname),
@@ -59,10 +82,9 @@ def tijana_eval_compute_gcm(G_df):
 
 
 def tijana_eval_compute_gcd(gcm_g, gcm_h):
-    if len(gcm_h) != len(gcm_g):
-        raise "Graphs must be same size"
+    assert len(gcm_h) == len(gcm_g), "Graphs must be same size"
     s = 0
-    for i in range(0, len(gcm_g)):
+    for i in range(len(gcm_g)):
         for j in range(i, len(gcm_h)):
             s += math.pow((gcm_g[i, j] - gcm_h[i, j]), 2)
 
