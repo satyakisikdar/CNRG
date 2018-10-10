56e3b5ffa6578b1e5f6ecdaa0a78bd3d5ad1d4da
Satyaki Sikdar <ssikdar@nd.edu>
2018-10-10 18:29:39 -0400

    added SUBDUE, VoG generators

---
+++ vrgs/other_graph_generators.py
@@ -0,0 +1,444 @@
+import networkx as nx
+import networkx.algorithms.bipartite
+from networkx.generators.classic import empty_graph
+import itertools
+import random
+import math
+import numpy as np
+import subprocess
+from time import time 
+
+
+def bter_wrapper(g):
+    print('Starting BTER...')
+
+    np.savetxt('./tmp/matlab/{}.mat'.format(g.name), nx.to_numpy_matrix(g), fmt='%d')
+
+    matlab_code = []
+    matlab_code.append("addpath('./tmp/matlab/');")
+    matlab_code.append("G = dlmread('{}.mat');".format(g.name))
+    matlab_code.append('G = sparse(G);')
+    matlab_code.append("graphname = '{}';".format(g.name))
+    matlab_code.append('')
+
+    matlab_code.append('nnodes = size(G, 1);')
+    matlab_code.append('nedges = nnz(G) / 2;')
+    matlab_code.append(r"fprintf('nodes: %d edges: %d\n', nnodes, nedges);")
+    matlab_code.append('')
+
+    matlab_code.append('nd = accumarray(nonzeros(sum(G,2)),1);')
+    matlab_code.append("maxdegree = find(nd>0,1,'last');")
+    matlab_code.append(r"fprintf('Maximum degree: %d\n', maxdegree);")
+    matlab_code.append('')
+
+    matlab_code.append('[ccd,gcc] = ccperdeg(G);')
+    matlab_code.append(r"fprintf('Global clustering coefficient: %.2f\n', gcc);")
+    matlab_code.append('')
+
+    matlab_code.append(r"fprintf('Running BTER...\n');")
+    matlab_code.append('t1=tic;')
+    matlab_code.append('[E1,E2] = bter(nd,ccd);')
+    matlab_code.append('toc(t1);')
+    matlab_code.append(r"fprintf('Number of edges created by BTER: %d\n', size(E1,1) + size(E2,1));")
+    matlab_code.append('')
+
+    matlab_code.append(r"fprintf('Turning edge list into adjacency matrix (including dedup)...\n');")
+    matlab_code.append('t2=tic;')
+    matlab_code.append('G_bter = bter_edges2graph(E1,E2);')
+    matlab_code.append('toc(t2);')
+    matlab_code.append(r"fprintf('Number of edges in dedup''d graph: %d\n', nnz(G)/2);")
+    matlab_code.append('')
+
+    matlab_code.append('G_bter = full(G_bter);')
+    matlab_code.append(r"dlmwrite('{}_bter.mat', G_bter, ' ');".format(g.name))
+
+    print('\n'.join(matlab_code), file=open('./tmp/matlab/{}_code.m'.format(g.name), 'w'))
+
+    start_time = time()
+    
+    completed_process = subprocess.run('cat ./tmp/matlab/{}_code.m | matlab'.format(g.name), shell=True)
+    
+    print('BTER ran in {} secs'.format(round(time() - start_time, 3)))
+
+    if completed_process.returncode != 0:
+        print('error in matlab')
+        return None
+
+    bter_mat = np.loadtxt('./tmp/matlab/{}_bter.mat'.format(g.name), dtype=int)
+
+    g_bter = nx.from_numpy_matrix(bter_mat, create_using=nx.Graph())
+    return g_bter
+
+
+def chung_lu(g):
+    print('Starting Chung-Lu....')
+
+    start_time = time()
+    
+    degree_seq = list(g.degree().values())
+    g_chung_lu = nx.expected_degree_graph(degree_seq, selfloops=False)
+    
+    print('Chung-Lu ran in {} secs'.format(round(time() - start_time, 3)))
+    return g_chung_lu
+
+
+def kronecker_graph(k, G):
+    I = nx.adjacency_matrix(G)
+
+    if G.is_directed():
+        return kronecker_random_graph(k, I, True)
+    else:
+        return kronecker_random_graph(k, I, False)
+
+
+def kronecker_random_graph(k, P, seed=None, directed=True):
+    """Return a random graph K_k[P] (Stochastic Kronecker graph).
+    Parameters
+    ----------
+    P : square matrix of floats
+        An n-by-n square "initiator" matrix of probabilities. May be a standard
+        Python matrix or a NumPy matrix.  If the graph is undirected,
+        must be symmetric.
+    k : int
+        The number of times P is Kronecker-powered, creating a stochastic
+        adjacency matrix.  The generated graph has n^k nodes,
+        where n is the dimension of P as noted above.
+    seed : int, optional
+        Seed for random number generator (default=None).
+    directed : bool, optional
+        If True, return a directed graph, else return an undirected one
+        (default=True).
+    Notes
+    -----
+    The stochastic Kronecker graph generation algorithm takes as input a
+    square matrix of probabilities, computes the iterated Kronecker power of
+    this matrix, and then uses the resulting stochastic adjacency matrix to
+    generate a graph. This algorithm is O(V^2), where V=n^k.
+    See Also
+    --------
+    kronecker2_random_graph
+    Examples
+    --------
+    >>> k=4
+    >>> P=[[0.8,0.3],[0.3,0.2]]
+    >>> G=nx.kronecker_random_graph(k,P)
+    >>> P=[[0.8,0.7],[0.3,0.2]]
+    >>> G=nx.kronecker_random_graph(k,P,directed=True)
+
+    References
+    ----------
+    .. [1] Jure Leskovec, Deepayan Chakrabarti, Jon Kleinberg, Christos Faloutsos,
+           and Zoubin Ghahramani,
+       "Kronecker graphs: an approach to modeling networks",
+       The Journal of Machine Learning Research, 11, 985-1042, 3/1/2010.
+    """
+    dim = len(P)
+
+    errorstring = ("The initiator matrix must be a nonempty" +
+                   (", symmetric," if not directed else "") +
+                   " square matrix of probabilities.")
+
+    if dim == 0:
+        raise nx.NetworkXError(errorstring)
+    for i, arr in enumerate(P):
+        if len(arr) != dim:
+            raise nx.NetworkXError(errorstring)
+        for j, p in enumerate(arr):
+            if p < 0 or p > 1:
+                raise nx.NetworkXError(errorstring)
+            if not directed and P[i][j] != P[j][i]:
+                raise nx.NetworkXError(errorstring)
+
+    if k < 1:
+        return empty_graph(1)
+
+    n = dim ** k
+    G = empty_graph(n)
+
+    if directed:
+        G = nx.DiGraph(G)
+
+    G.add_nodes_from(range(n))
+    G.name = "kronecker_random_graph(%s,%s)" % (n, P)
+
+    if not seed is None:
+        random.seed(seed)
+
+    if G.is_directed():
+        edges = itertools.product(range(n), range(n))
+    else:
+        edges = itertools.chain([(v, v) for v in range(n)], itertools.combinations(range(n), 2))
+
+    for e in edges:
+        row, col = e
+        p = 1.0
+        initPow = 1
+        for i in range(k):
+            rowVal = (row // initPow) % dim
+            colVal = (col // initPow) % dim
+            p = p * (P[rowVal][colVal])
+            initPow = initPow * dim
+        if random.random() < p:
+            G.add_edge(*e)
+
+    return G
+
+
+def kronecker2_random_graph(k, P, seed=None, directed=True):
+    r"""Return a sparse random graph K_k[P] (Stochastic Kronecker graph).
+    Parameters
+    ----------
+    P : square matrix of floats
+        An n-by-n square "initiator" matrix of probabilities. May be a standard
+        Python matrix or a NumPy matrix.  If the graph is undirected,
+        must be symmetric.
+    k : int
+        The number of times P is Kronecker-powered, creating a stochastic
+        adjacency matrix.  The generated graph has n^k nodes,
+        where n is the dimension of P as noted above.
+    seed : int, optional
+        Seed for random number generator (default=None).
+    directed : bool, optional
+        If True, return a directed graph, else return an undirected one
+        (default=True).
+    Notes
+    -----
+    The stochastic Kronecker graph generation algorithm takes as input a
+    square matrix of probabilities, computes the iterated Kronecker power of
+    this matrix, and then uses the resulting stochastic adjacency matrix to
+    generate a graph.
+    This "fast" algorithm runs in O(E) time. It thus works best when the expected
+    number of edges in the graph is roughly O(V).
+    The expected number of edges in the graph is given by d^k, where
+    d=\sum_{i,j} P[i,j] is the sum of all the elements in P.
+    See Also
+    --------
+    kronecker_random_graph
+    Examples
+    --------
+    >>> k=4
+    >>> P=[[0.8,0.3],[0.3,0.2]]
+    >>> G=nx.kronecker2_random_graph(k,P)
+
+    References
+    ----------
+    .. [1] Jure Leskovec, Deepayan Chakrabarti, Jon Kleinberg, Christos Faloutsos,
+           and Zoubin Ghahramani,
+       "Kronecker graphs: an approach to modeling networks",
+       The Journal of Machine Learning Research, 11, 985-1042, 3/1/2010.
+    """
+    print('Starting Kronecker....')
+
+    dim = len(P)
+
+    errorstring = ("The initiator matrix must be a nonempty" +
+                   (", symmetric," if not directed else "") +
+                   " square matrix of probabilities.")
+
+    if dim == 0:
+        raise nx.NetworkXError(errorstring)
+    for i, arr in enumerate(P):
+        if len(arr) != dim:
+            raise nx.NetworkXError(errorstring)
+        for j, p in enumerate(arr):
+            if p < 0 or p > 1:
+                raise nx.NetworkXError(errorstring)
+            if not directed and P[i][j] != P[j][i]:
+                raise nx.NetworkXError(errorstring)
+
+    if k < 1:
+        return empty_graph(1)
+
+    n = dim ** k
+    G = empty_graph(n)
+    G = nx.DiGraph(G)
+
+    acc = 0.0
+    partitions = []
+    for i in range(dim):
+        for j in range(dim):
+            if P[i][j] != 0:
+                acc = acc + P[i][j]
+                partitions.append([acc, i, j])
+    psum = acc
+
+    G.add_nodes_from(range(n))
+    G.name = "kronecker2_random_graph(%s,%s)" % (n, P)
+
+    if not seed is None:
+        random.seed(seed)
+
+    expected_edges = math.floor(psum ** k)
+    num_edges = 0
+    while num_edges < expected_edges:
+        multiplier = dim ** k
+        x = y = 0
+        for i in range(k):
+            multiplier = multiplier // dim
+            r = c = -1
+            p = random.uniform(0, psum)
+            for n in range(len(partitions)):
+                if partitions[n][0] >= p:
+                    r = partitions[n][1]
+                    c = partitions[n][2]
+                    break
+            x = x + r * multiplier
+            y = y + c * multiplier
+
+        if not G.has_edge(x, y):
+            G.add_edge(x, y)
+            num_edges = num_edges + 1
+
+    if not directed:
+        G = G.to_undirected()
+
+    return G
+
+
+def subdue(g):
+    print('Starting SUBDUE....')
+
+    name = g.name
+    g = nx.convert_node_labels_to_integers(g, first_label=1)
+    g.name = name
+
+    with open('./subdue/{}_subdue.g'.format(g.name), 'w') as f:
+        for u in sorted(g.nodes_iter()):
+            f.write('\nv {} v'.format(u))
+
+        for u, v in g.edges_iter():
+            f.write('\nu {} {} e'.format(u, v))
+
+    start_time = time()
+    completed_process = subprocess.run('cd subdue;./subdue -undirected -nsubs 100000000 {}_subdue.g'.format(g.name),
+                                       shell=True, stdout=subprocess.PIPE)
+
+    print('SUBDUE ran in {} secs'.format(round(time() - start_time, 3)))
+
+    if completed_process.returncode != 0:
+        return None
+
+    structures = []
+    raw_st = completed_process.stdout.decode("utf-8")
+
+    lines = raw_st.split('\n')
+
+    for i, line in enumerate(lines):
+        if line.startswith('('):  # start of a substructure
+            sub_count = int(line.split(',')[-2].split()[-1])  # freq of substructure
+            substr = nx.Graph()
+            next_line = lines[i + 1]
+            n = int(next_line[next_line.find('(') + 1: next_line.find('v')])  # number of nodes
+            m = int(next_line[next_line.find(',') + 1: next_line.find('e')])  # number of edges
+            for j in range(i + 2, i + 2 + m + n):
+                if lines[j].strip().startswith('u'):  # it's an edge
+                    u, v = map(int, lines[j].split()[1: 3])
+                    substr.add_edge(u, v)
+
+            structures.append((substr, sub_count))
+
+    return structures
+
+
+def vog(g):
+    print('Starting VoG....')
+
+    name = g.name
+    g = nx.convert_node_labels_to_integers(g, first_label=1)
+
+    with open('./vog/DATA/{}.g'.format(name), 'w') as f:
+        for u, v in g.edges_iter():
+            f.write('\n{} {} 1'.format(u, v))
+
+
+    matlab_code = []
+    matlab_code.append("addpath('DATA');")
+    matlab_code.append("addpath('STRUCTURE_DISCOVERY');")
+
+
+    matlab_code.append("input_file = './DATA/{}.g';".format(name))
+
+    matlab_code.append("unweighted_graph = input_file;")
+    matlab_code.append("output_model_greedy = 'DATA';")
+    matlab_code.append("output_model_top10 = 'DATA';")
+        
+    matlab_code.append("orig = spconvert(load(input_file));")
+    matlab_code.append("orig(max(size(orig)),max(size(orig))) = 0;")
+    matlab_code.append("orig_sym = orig + orig';")
+    matlab_code.append("[i,j,k] = find(orig_sym);")
+
+    matlab_code.append("orig_sym(i(find(k==2)),j(find(k==2))) = 1;")
+    matlab_code.append("orig_sym_nodiag = orig_sym - diag(diag(orig_sym));")
+
+    matlab_code.append("disp('==== Running VoG for structure discovery ====')")
+    matlab_code.append("global model;")
+    matlab_code.append("model = struct('code', {}, 'edges', {}, 'nodes1', {}, 'nodes2', {}, 'benefit', {}, 'benefit_notEnc', {});")
+    matlab_code.append("global model_idx;")
+    matlab_code.append("model_idx = 0;")
+    matlab_code.append("SlashBurnEncode( orig_sym_nodiag, 2, output_model_greedy, false, false, 3, unweighted_graph);")
+
+    matlab_code.append("quit;")
+
+    print('\n'.join(matlab_code), file=open('./vog/{}_vog_code.m'.format(name), 'w'))
+
+    start_time = time()
+    completed_process = subprocess.run('cd vog; matlab -r {}_vog_code'.format(name), shell=True)
+    print('VoG ran in {} secs'.format(round(time() - start_time, 3)))
+
+
+    if completed_process.returncode != 0:
+        print('error in matlab')
+        return None
+
+    with open('./vog/DATA/{}_ALL.model'.format(name)) as f:
+        raw_st = f.read()
+
+    structures = []
+    for line in raw_st.split('\n'):
+
+        if line.startswith('fc'):  # a full clique "fc 1 2 3 4" => a clique on 4 nodes
+            n = len(line.split()[1:])
+            g_struct = nx.complete_graph(n)
+
+        elif line.startswith('nc'):  # near clique "nc 5, 1 2 3 4" => a near clique over nodes 1-4 with 5 edges between them
+            line = line.replace(',', '')
+            m = int(line.split()[1])
+            nodes = map(int, line.split()[2:])
+            g_struct = g.subgraph(nodes)
+
+        elif line.startswith('ch'):  # chain "ch 4 2 1 3" => 4-2-1-3
+            n = len(line.split()[1:])
+            g_struct = nx.path_graph(n)
+
+        elif line.startswith('st'):  # star "st 1, 2 3 4" => star with 1 as hub with edges to 2, 3, 4
+            line = line.replace(',', '')
+            n_outer_nodes = len(line.split()[2:])
+            g_struct = nx.star_graph(n_outer_nodes)
+
+        elif line.startswith('bc'):  # bipartite core "bc 1 2 3, 4 5" complete bipartite graph between {1, 2, 3} and {4, 5}
+            first_half, second_half = line.split(',')
+            n1 = len(first_half.split()[1:])
+            n2 = len(second_half.split())
+            g_struct = networkx.bipartite.complete_bipartite_graph(n1, n2)
+
+        elif line.startswith('nb'):  # near bipartite core "nb 1 2 3, 4 5"
+            first_half, second_half = line.split(',')
+            nodes_1 = set(map(int, first_half.split()[1:]))
+            nodes_2 = set(map(int, second_half.split()))
+
+            g_fb = nx.Graph()
+            [g_fb.add_edge(u, v) for u in nodes_1 for v in nodes_2]  # making a complete biparite graph
+            edges = set(g.edges_iter()) & set(
+                g_fb.edges_iter())  # pick only those edges which actually exist in the graph
+
+            g_struct = nx.Graph()
+            g_struct.add_edges_from(edges)
+
+        else:
+            continue
+
+        g_struct.name = line
+        structures.append(g_struct)
+
+    return structures
+
--- vrgs/runner.py
+++ vrgs/runner.py
@@ -15,13 +15,17 @@ from copy import deepcopy
 import os
 import sys
 import numpy as np
+import math
 
 sys.path.extend([os.getcwd(), os.path.dirname(os.getcwd())])
 
 import vrgs.partitions as partitions
 from vrgs.funky_extract import funky_extract
 from vrgs.Tree import create_tree
-
+import vrgs.analysis as analysis
+from vrgs.MDL import graph_mdl
+from vrgs.other_graph_generators import chung_lu, kronecker_random_graph, bter_wrapper, vog, subdue
+from vrgs.GCD import GCD
 
 def get_graph(filename='sample'):
     if filename == 'sample':
@@ -43,28 +47,71 @@ def get_graph(filename='sample'):
         g = nx.convert_node_labels_to_integers(g)
     return g
 
+def write_stats():
+    fieldnames = ('name', 'k', 'count',
+                  'actual_n', 'actual_m', 'actual_MDL',
+                  'full_n', 'full_m', 'full_rules', 'full_MDL', 'GCD_full', 'CVM_full_d', 'CVM_full_pr',
+                  'part_n', 'part_m', 'part_rules', 'part_MDL', 'GCD_part', 'CVM_part_d', 'CVM_part_pr',
+                  'no_n', 'no_m', 'no_rules', 'no_MDL', 'GCD_no', 'CVM_no_d', 'CVM_no_pr')
 
-def get_rule_graph(rule_dict):
-    g = nx.DiGraph()
+    names = ('karate', 'lesmis', 'football', 'eucore', 'GrQc', 'gnutella', 'wikivote')
 
-    stack = [0]
-    visited = set()
+    for name in names[: 1]:
+        stats_filename = './stats/{}_stats.csv'.format(name)
+        with open(stats_filename, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
 
-    while len(stack) != 0:
-        u = stack.pop()
-        for rule in rule_dict[u]:
-            for node, d in rule.graph.nodes_iter(data=True):
-                if 'label' in d:
-                    v = d['label']
+        print()
+        print(name)
 
-                    if v not in visited:
-                        visited.add(v)
-                        g.add_edge(u, v)
-                        stack.append(v)
-    print(g.edges())
-    print('cycles', len(list(nx.simple_cycles(g))))
-    return g
+        g = get_graph('./tmp/{}.g'.format(name))
+        g.name = name
+        # g = get_graph()
+        orig_graph = deepcopy(g)
+        g_mdl = graph_mdl(orig_graph)
 
+        tree = partitions.approx_min_conductance_partitioning(g)
+        # tree = [[[[6], [7]], [[8], [9]]], [[[5], [[3], [4]]], [[1], [2]]]]
+
+        root = create_tree(tree)
+        new_graph_count = 2  # number of graphs generated
+
+        for k in range(3, 5):
+            print('\nk =', k)
+
+            row = {'name': name, 'k': k, 'actual_n': orig_graph.order(), 'actual_m': orig_graph.size(),
+                   'actual_MDL': g_mdl}
+            rows = [dict() for _ in range(new_graph_count)]  # for each of the row, this info is the same
+
+            for mode in ('full', 'part', 'no'):
+                grammar = funky_extract(g=deepcopy(g), root=deepcopy(root), k=k, mode=mode, pick_randomly=True)
+                print('\n"{}" original graph {} nodes {} edges'.format(mode.upper(), orig_graph.order(),
+                                                                       orig_graph.size()))
+
+                graphs = grammar.generate_graphs(count=new_graph_count)
+                row['{}_rules'.format(mode)] = len(grammar)
+                row['{}_MDL'.format(mode)] = grammar.get_cost()
+
+                for i in range(new_graph_count):
+                    rows[i] = {**row, **rows[i]}
+
+                for count, g1 in enumerate(graphs):
+                    rows[count]['count'] = count
+                    rows[count]['{}_n'.format(mode)] = g1.order()
+                    rows[count]['{}_m'.format(mode)] = g1.size()
+
+                    gcd, cvm_deg, cvm_page = analysis.compare_two_graphs(orig_graph, g1)
+                    rows[count]['GCD_{}'.format(mode)] = gcd
+                    rows[count]['CVM_{}_d'.format(mode)] = cvm_deg
+                    rows[count]['CVM_{}_pr'.format(mode)] = cvm_page
+
+            assert len(rows[0]) == len(fieldnames)
+            with open(stats_filename, 'a') as f:
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                for row in rows:
+                    writer.writerow(row)
+                writer.writerow({})  # a blank row
 
 def main():
     """
@@ -72,39 +119,56 @@ def main():
     :return:
     """
     np.seterr(all='ignore')
-
-
     # networkx to matrix
     # np.savetxt('karate.mat', nx.to_numpy_matrix(g), fmt='%d')
-    # columns = ['name', 'k', 'count',
-    #            'actual_n', 'actual_m', 'actual_MDL',
-    #            'full_n', 'full_m', 'full_rules', 'full_MDL', 'GCD_full', 'CVM_full_d', 'CVM_full_pr',
-    #            'part_n', 'part_m', 'part_rules', 'part_MDL', 'GCD_part', 'CVM_part_d', 'CVM_part_pr',
-    #            'no_n', 'no_m', 'no_rules', 'no_MDL', 'GCD_no', 'CVM_no_d', 'CVM_no_pr']
 
-    names = ('karate', 'lesmis', 'football', 'eucore', 'GrQc', 'gnutella', 'wikivote')
+    name = 'football'
+    g = get_graph('./tmp/{}.g'.format(name))
+    g.name = name
+    # g = get_graph()
 
-    for name in names[: 3]:
-        print()
-        print(name)
+    g_chung_lu = chung_lu(g)
+    print('n: {} m: {} n: {}, m: {} GCD {}'.format(g.order(), g.size(), g_chung_lu.order(), g_chung_lu.size(),
+                                                 GCD(g, g_chung_lu)))
+    
+    k = int(math.log(g.size(), 2))
 
-        g = get_graph('./tmp/{}.g'.format(name))
-        # g = get_graph()
-        orig_graph = deepcopy(g)
+    g_kron = kronecker2_random_graph(k, [[0.99, 0.6683], [0.6683, 0.025]], directed=False)
+    
+    print('n: {} m: {} n: {}, m: {} GCD {}'.format(g.order(), g.size(), g_kron.order(), g_kron.size(),
+                                                 GCD(g, g_kron)))
 
-        tree = partitions.approx_min_conductance_partitioning(g)
-        # tree = [[[[6], [7]], [[8], [9]]], [[[5], [[3], [4]]], [[1], [2]]]]
+    structures = subdue(g)
 
-        root = create_tree(tree)
+    print('{} structures discovered by SUBDUE'.format(len(structures)))
 
-        for k in range(3, 7):
-            print('\nk =', k)
+    structures = vog(g)
 
-            for mode in ('full', 'part', 'no'):
-                grammar = funky_extract(g=deepcopy(g), root=deepcopy(root), k=k, mode=mode, pick_randomly=True)
-                print('\n"{}" original graph {} nodes {} edges'.format(mode, orig_graph.order(), orig_graph.size()))
+    print('{} structures discovered by VoG'.format(len(structures)))
 
-                graphs = grammar.generate_graphs(count=5)
+    return 
+
+    g_bter = bter_wrapper(g)
+    print('n: {} m: {} n: {}, m: {} GCD {}'.format(g.order(), g.size(), g_bter.order(), g_bter.size(),
+                                                 GCD(g, g_bter)))
+
+    return
+
+    orig_graph = deepcopy(g)
+    tree = partitions.approx_min_conductance_partitioning(g)
+    # tree = [[[[6], [7]], [[8], [9]]], [[[5], [[3], [4]]], [[1], [2]]]]
+
+    root = create_tree(tree)
+    new_graph_count = 2  # number of graphs generated
+
+    k = 4
+
+    for mode in ('full', 'part', 'no'):
+        grammar = funky_extract(g=deepcopy(g), root=deepcopy(root), k=k, mode=mode, pick_randomly=True)
+        print('\n"{}" original graph {} nodes {} edges'.format(mode.upper(), orig_graph.order(),
+                                                               orig_graph.size()))
+
+        graphs = grammar.generate_graphs(count=new_graph_count)
 
 
 
